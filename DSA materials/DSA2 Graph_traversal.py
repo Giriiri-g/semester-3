d04dd68dab6b3e8fb2b8441c5265b70c6cfcf773
callme-X <giri090205@gmail.com>
2023-12-15 21:04:26 +0530

    dsa dijkstra commit

--- DSA materials/DSA2 Graph_traversal.py
+++ DSA materials/DSA2 Graph_traversal.py
@@ -71,13 +71,46 @@ dfs(graph)
 ## [ Dijkstra's Algorithm ]
 
 
-graph2 = [
-[0, 2, 4, None, None, None],
-[None, 0, 1, 4, None, None],
-[None, None, 0, None, 3, None],
-[None, None, None, 0, None, 1],
-[None, None, None, 2, 0, 5],
-[None, None, None, None, None, None]]
+graph = {
+'A':{'B':2, 'C':3, 'D':1},
+'B':{'D':2},
+'C':{'E':1, 'D':3},
+'D':{'E':2},
+'E':{},
+'S':{'A':1, 'B':5},
+}
 
-def dijkstra(matrix):
-     pass
+def dijkstra(adjlist):
+     cost = {'A':0, 'B':None, 'C':None, 'D':None, 'E':None, 'S':None}
+     min_ = {'A':0, 'B':None, 'C':None, 'D':None, 'E':None, 'S':None}
+     Node = 'A'
+     print(' Node | B C D E S')
+     print('   A  | ∞ ∞ ∞ ∞ ∞')
+     while True:
+          child = adjlist[Node]
+          for i in child:
+               if cost[Node] in [None, '-']:
+                    continue
+               if cost[i] is None or cost[Node]+ child[i] < cost[i]:
+                    cost[i] = cost[Node] + child[i]
+          # Node = min(cost, key=lambda k: float('inf') if cost[k] is None or cost[k] == '_' else cost[k])
+          minNode = ['-', 0]
+          for i in cost:
+               if cost[i] in [None, '-']:
+                    continue
+               if cost[i]<=minNode[1]:
+                    minNode[1] = cost[i]
+                    minNode[0] = i
+                    
+          Node = minNode[0]
+          min_[Node] = minNode[1]
+          cost[Node] = '-'
+          print(' {:^4} | {} {} {} {} {}'.format(Node, cost['B'], cost['C'], cost['D'], cost['E'], cost['S']))
+          if all(c == '-' for c in cost.values()):
+               break
+     print("The Minimum cost to travel to Nodes from A:")
+     for i in min_:
+          print(f"{i} : {min_[i]}")
+     return min_
+
+dijkstra(graph)
