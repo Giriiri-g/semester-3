03d5efbeb7b127f45e426b0f193180a5e5516419
callme-X <giri090205@gmail.com>
2023-12-17 23:28:48 +0530

    IPY lab 2 update

--- IPY materials/IPY lab 1.py
+++ IPY materials/IPY lab 1.py
@@ -191,3 +191,37 @@ def pattern3(n):
           for j in range(i, 0, -1):
                print(j, end="")
           print("")
+
+
+def pattern4(n):
+     for i in range(n, 0, -1):
+          for j in range(1, i+1):
+               print(j, end="")
+          print("")
+
+
+def pattern5(n):
+     for i in range(1, n+1):
+          print(" "*(n-i), end="")
+          for j in range(1, i+1):
+               print(j, end="")
+          for j in range(i-1, 0, -1):
+               print(j, end="")
+          print("")
+
+
+def pattern6(n):
+     for i in range(1, n//2+2):
+          print(" "*(n//2+1-i), end="")
+          for j in range(1, i+1):
+               print(j, end="")
+          for j in range(i-1, 0, -1):
+               print(j, end="")
+          print("")
+     for i in range(n//2, 0, -1):
+          print(" "*(n//2+1-i), end="")
+          for j in range(1, i+1):
+               print(j, end="")
+          for j in range(i-1, 0, -1):
+               print(j, end="")
+          print("")
--- IPY materials/IPY lab 2.py
+++ IPY materials/IPY lab 2.py
@@ -13,8 +13,6 @@ def revnum(n):
 ## [Question 3]
 
 def fact(x):
-    if isinstance(x, str) == True:
-        raise ValueError ("invalid literal for fact() with base 10")
     if isinstance(x, int) == False:
         raise ValueError ("invalid literal for fact() with base 10")
     l = []
@@ -25,7 +23,6 @@ def fact(x):
 
 
 ## [Question 4]
-
 
 def fct_(x):
     if x==0:
@@ -58,6 +55,118 @@ def calculate_series_sum(x, n):
 
 
 ## [Question 6]
-
+     
 def costcalc(product_cost, tax_rate=0.07, shipping_cost=5):
      return product_cost + tax_rate + shipping_cost
+
+
+## [Question 7]
+
+def calculate_volume(length, width, height):
+     return length*width*height
+
+
+## [Question 8]
+
+item_count=0
+
+def add_item_to_cart():
+     global item_count
+     item_count+=1
+     print("Item added to shopping cart")
+
+
+def view_shopping_cart():
+     global item_count
+     print(f"The Number of items in the shopping cart is {item_count}.")
+
+
+# main program
+
+add_item_to_cart()
+add_item_to_cart()
+add_item_to_cart()
+view_shopping_cart()
+
+
+## [Question 9]
+
+def calcBaseTax(income):
+     if income>100000:
+          return income*0.3
+     if income>50000:
+          return income*0.2
+     if income>10000:
+          return income*0.1
+     return income*0.05
+
+
+def calcTaxBonus(age, num_child=None):
+     bonus=0
+     if age>65:
+          bonus+=500
+     if num_child is not None:
+          bonus += 200*num_child
+     return bonus
+
+
+def calcTax(income, age, num_child=None):
+     Base_Tax = calcBaseTax(income)
+     deductions = calcTaxBonus(age, num_child)
+     incomeTax = Base_Tax - deductions
+     afterIncomeTax = income - incomeTax
+     print(f"Your Base income Tax is ${Base_Tax:.2f}.")
+     print(f"Dependecies Deductions: ${deductions:.2f}")
+     print(f"Total Income Tax      : ${incomeTax:.2f}")
+     print(f"Income after Tax      : ${afterIncomeTax:.2f}")
+     return afterIncomeTax, incomeTax
+     
+
+## [Question 10]
+
+def pizzaDelivery():
+     confirm = '0'
+     while confirm!='1':
+          print("Welcome to Digital Pizza Delivery Services")
+          size = input("Enter pizza size (S, M, L): ").upper()
+          if size not in ['S', 'M', 'L']:
+               print("Invalid Size: Please choose from small(S), Medium(M), Large(L)")
+               continue
+          cheese = input("Do you want extra cheese? (Y or N): ").upper()
+          if cheese not in ['Y', 'N']:
+               print("Invalid choice: Please choose from Yes(Y) or No(N)")
+               continue
+          cheese = cheese=='Y'
+          pepperoni = input("Do you want pepperoni? (Y or N): ").upper()
+          if pepperoni not in ['Y', 'N']:
+               print("Invalid choice: Please choose from Yes(Y) or No(N)")
+               continue
+          pepperoni = pepperoni=='Y'
+
+          print(f"Order Confirmation:\nPizza Size: {size}\nExtra Cheese: {cheese}\nPepperoni: {pepperoni}")
+          confirm = input("Press 1 to confirm your order: ")
+     cost = calcCost(size, cheese, pepperoni)
+     Bill(size, cheese, pepperoni, cost)
+
+
+def calcCost(size, cheese, pepperoni):
+     sizecost = {'S': 8, 'M': 10, 'L': 12}
+     cost = sizecost[size]
+     if cheese:
+          cost+=1
+     if pepperoni:
+          cost+=2
+     return cost
+
+
+def Bill(size, cheese, pepperoni, cost):
+     print("\nOrder Summary:")
+     print(f"Pizza Size   : {size}")
+     print(f"Extra Cheese : {cheese}")
+     print(f"Pepperoni    : {pepperoni}")
+     print(f"Total Cost   : ${cost:.2f}")
+
+pizzaDelivery()
+
+
+          
