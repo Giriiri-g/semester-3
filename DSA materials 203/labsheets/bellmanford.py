491e6355b7455c298c0886105b39ea26fd0a6f93
callme-X <giri090205@gmail.com>
2024-01-01 10:22:56 +0530

    DSA lab3 push

--- DSA materials 203/labsheets/bellmanford.py
+++ DSA materials 203/labsheets/bellmanford.py
@@ -4,11 +4,6 @@ class Graph:
      def __init__(self, adj_dic):
           self.adj_dic = adj_dic
 
-     def Edge(self, u, v):
-          if v in self.adj_dic[u]:
-               return self.adj_dic[u][v]
-          return None
-
      def child(self, parent):
           return list(self.adj_dic[parent])
 
@@ -31,7 +26,7 @@ dic = {
 'A':{'B':6, 'C':4, 'D':5},
 'B':{'E':-1},
 'C':{'E':3, 'B':-2},
-'D':{'E':-2, 'F':-1},
+'D':{'C':-2, 'F':-1},
 'E':{'F':3},
 'F':{},
 }
@@ -42,23 +37,22 @@ graph = Graph(dic)
 
 ## [ Bellman-Ford ]
 
-def Bellman(Graph, source):
-     dist={}
-     for vertex in Graph.vertices():
-          dist[vertex] = float('inf')
+def Bellman(Graph, source, _help_=False):
+     dist={vertex: float('inf') for vertex in Graph.vertices()}
      dist[source] = 0
 
      iter_ = len(Graph.vertices())
-     
-
+     if _help_:
+          print(0, dist)
      for i in range(iter_-1):
-          dist_dup = dist
+          dist_dup = dist.copy()
           for path in Graph.Edges():
                if dist[path[0]] + Graph.PathWeight(path[0], path[1]) < dist[path[1]]:
                     dist[path[1]] = dist[path[0]] + Graph.PathWeight(path[0], path[1])
-
-
-                    
+          if _help_:
+               print(i+1, dist)
           if dist_dup == dist:
+               print("Break: No Updation!")
                return dist
      return dist
+Bellman(graph, "A", True)
