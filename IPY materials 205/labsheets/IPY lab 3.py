3a47eee70625f0fe482a367c469d8bcffa516364
callme-X <giri090205@gmail.com>
2024-01-02 11:55:37 +0530

    IPY lab3 push

--- IPY materials 205/labsheets/IPY lab 3.py
+++ IPY materials 205/labsheets/IPY lab 3.py
@@ -98,3 +98,122 @@ l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(f"List before swapping: {l}\nList after swapping: {ListHalfswap(l)}")
 l = [1, 2, 3, 4, 5, 6, 7, 8]
 print(f"List before swapping: {l}\nList after swapping: {ListHalfswap(l)}")
+
+
+## [ Question 5 ]
+
+
+def peak(l):
+     return max(l)
+
+def peak2(l):
+     if len(l)<=3:
+          return peak(l)
+     if l[0]>l[1]:
+          return l[0]
+     if l[-1]> l[-2]:
+          return l[-1]
+     for i in range(1, len(l)-1):
+          if l[i] == max(l[i-1], l[i], l[i+1]):
+               return l[i]
+
+
+l= [10, 20, 15, 2, 23, 90, 67]
+print(f"Peak of the List {l} is {peak(l)}")
+print(f"Peak of the List {l} is {peak2(l)}")
+
+
+## [ Question 6 ]
+
+def list2(l):
+     l2 = [i for i in l if l.count(i)==1]
+     l3 = list(set([i for i in l if l.count(i)!=1]))
+     return l2, l3
+
+l = [1, 2, 3, 1, 2, 5, 4, 6, 3, 5, 0, 4, 8, 9 ,7, 10]
+l2, l3 = list2(l)
+print(f"The Unique values of List l={l} are L2={l2}\nand Non Unique Values of the List are L3={l3}.")
+
+
+## [ Question 7 ]
+
+def rotate(l, x):
+     return l[len(l)-x:] + l[:len(l)-x]
+
+l = [1, 2, 3, 4, 5, 6]
+x=2
+desired_output = [5, 6, 1, 2, 3, 4]
+print(f"The List l={l} has been rotated {x} times and obtained: {rotate(l, x)}\nwhich is the same as the desired Output: {desired_output}.")
+
+
+## [ Question 8 ]
+
+
+def sumpair(l1, l2, sum_):
+     pairs=[]
+     for i in l1:
+          for j in l2:
+               if i+j == sum_:
+                    pairs.append((i, j))
+     return pairs
+
+list1 = [-1, -2, 4, -6, 5, 7]
+list2 = [6, 3, 4, 0] 
+x = 8
+
+res = sumpair(list1, list2, x)
+
+print(f"The Pairs of sums of elements from the Lists {list1} and {list2} are:\n{res}")
+
+
+## [ Question 9 ]
+
+def disp(mat):
+     for i in mat:
+          for j in i:
+               print("{:<7}".format(j), end="")
+          print()
+
+mat = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+print("Matrix Display:")
+disp(mat)
+
+def sumrow(mat):
+     arr=[]
+     for i in mat:
+          arr.append([sum(i)])
+     return arr
+
+print("\n\n\nSum of Rows:")
+
+disp(sumrow(mat))
+
+
+def sumcol(mat):
+     arr=[]
+     for i in range(len(mat[0])):
+          arr.append(sum([j[i] for j in mat]))
+     return [arr]
+
+print("\n\n\nSum of Columns:")
+disp(sumcol(mat))
+
+
+def summat(mat1, mat2):
+     mat=[]
+     for i, j in enumerate(mat1):
+          row=[]
+          for k, l in enumerate(j):
+               row.append(l+mat2[i][k])
+          mat.append(row)
+     return mat
+
+mat1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+mat2 = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+print("\n\nMatrix 1:")
+disp(mat1)
+print("\n\nMatrix 2")
+disp(mat2)
+print("\n\n\nSum of Matrices")
+disp(summat(mat1, mat2))
