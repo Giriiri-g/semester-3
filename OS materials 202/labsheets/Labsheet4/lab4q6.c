6d78039d7fea20eff4a150fd87d16642e14c715e
Giriiri-g <giri090205@gmail.com>
2024-02-07 01:40:44 +0530

    OS lab 4 update q6

--- OS materials 202/labsheets/Labsheet4/lab4q6.c
+++ OS materials 202/labsheets/Labsheet4/lab4q6.c
@@ -3,21 +3,18 @@
 #include <sys/wait.h>
 
 int main(){
-    pid_t first_child, second_child;
-    first_child=fork();
-
-    if(first_child==0){
-       execl("./happynewyear","./happynewyear", NULL);
-      }else{
-        wait(NULL);
-        second_child=fork();
-
-        if(second_child==0){
-              execl("./sum","./sum",NULL);
-        } else{
-             wait(NULL);
-             printf("Parent process exiting\n");
-        }
-}
-return 0;
+    if(!fork()){ // first child
+		execl("./happynewyear","./happynewyear", NULL);
+	}
+	else{
+		wait(NULL);
+		if(!fork()){ // second child waits until first child finishes
+			execl("./sum","./sum",NULL);
+		}
+		else{ // parent waits until both children finishes
+			wait(NULL);
+			printf("Parent process exiting");
+		}
+	}
+	return 0;
 }
